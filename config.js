ad0b8b6516497a06f35b34b8d5b0a10b414e9042
shijy16 <745921992@qq.com>
2021-11-05 21:17:15 +0800

    add configs

---
+++ config.js
@@ -0,0 +1,11 @@
+{
+    "pet_path": "pets/naruto",
+    "timer": [
+        [ "drink water and rest eyes", 40 ],
+        [ "stand up", 90 ]
+    ],
+    "plan": [
+        [ "book time", 12.0 ],
+        [ "book time", 18.0 ]
+    ]
+}
--- pet_egg.py
+++ pet_egg.py
@@ -3,15 +3,39 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets, QtGui
 import json
+import sys
+
+config_path = 'config.js'
+
+def write_config():
+    config = {}
+    config['pet_path'] = 'pets/naruto'
+    config['timer'] = []
+    config['timer'].append(['drink water and rest eyes', 40])
+    config['timer'].append(['stand up', 90])
+    config['plan'] = []
+    config['plan'].append(['book time', 12.0])
+    config['plan'].append(['book time', 18.0])
+    with open(config_path, 'w') as f:
+        json.dump(config, f, indent=4)
 
 class Pet(QWidget):
     def __init__(self, parent=None, **kwargs):
         super(Pet, self).__init__(parent)
-        self.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowStaysOnTopHint|Qt.SubWindow)
-        self.setAutoFillBackground(False)
-        self.setAttribute(Qt.WA_TranslucentBackground, True)
-        self.repaint()
+        self.config_path = kwargs['config']
+        self.read_configs()
+        self.show()
+        # self.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowStaysOnTopHint|Qt.SubWindow)
+        # self.setAutoFillBackground(False)
+        # self.setAttribute(Qt.WA_TranslucentBackground, True)
+        # self.repaint()
 
     def read_configs(self):
-        self.configs = 
+        with open(self.config_path, 'r') as f:
+            self.configs = json.load(f)
+        self.pet_path = self.configs['pet_path']
+        
+
 if __name__ == '__main__':
+    app = QApplication(sys.argv)  
+    pet = Pet(config=config_path)
